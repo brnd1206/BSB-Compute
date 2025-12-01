2037c6c96f3aef62602fd425d3023314446ae444
Bernardo Leite <bernardocar1206@gmail.com>
2025-12-01 20:32:49 -0300

    estrutura da classe Orquestrador e inicializacao dos processo

--- bsb_compute_app.py
+++ bsb_compute_app.py
@@ -72,3 +72,49 @@ def processo_servidor(id_servidor, capacidade, fila_tarefas, fila_resultados):
     tempo_total = (time.time() - inicio_log) / ESCALA_TEMPO
     pct_uso = (tempo_ocupado / tempo_total) * 100 if tempo_total > 0 else 0
     fila_resultados.put({"id_servidor": id_servidor, "status": "desligado", "uso": pct_uso})
+
+class Orquestrador:
+    def __init__(self, dados_servidores, dados_tarefas, politica, modo_silencioso=False):
+        self.politica = politica
+        self.modo_silencioso = modo_silencioso
+        self.fila_resultados = multiprocessing.Queue()
+        self.servidores = []
+        self.tarefas_pendentes = []
+
+        # Prepara tarefas com tempos de chegada aleatórios
+        tempo_atual = time.time()
+        for t in dados_tarefas:
+            atraso_chegada = random.uniform(0, 3) # Simula chegada dinâmica
+            self.tarefas_pendentes.append(Tarefa(
+                id=t['id'],
+                tipo=t['tipo'],
+                prioridade=t['prioridade'],
+                tempo_exec=t['tempo_exec'],
+                chegada=tempo_atual + atraso_chegada
+            ))
+
+        # Ordena inicial por chegada
+        self.tarefas_pendentes.sort(key=lambda x: x.chegada)
+
+        # Inicializa Processos (Workers)
+        self.processos = []
+        for s in dados_servidores:
+            q = multiprocessing.Queue()
+            p = multiprocessing.Process(
+                target=processo_servidor,
+                args=(s['id'], s['capacidade'], q, self.fila_resultados)
+            )
+            self.servidores.append(InfoServidor(s['id'], s['capacidade'], q))
+            self.processos.append(p)
+            p.start()
+
+        if not self.modo_silencioso:
+            print(f"--- BSB Compute: Iniciando Simulação Real ({self.politica.name}) ---")
+
+    def _log(self, mensagem):
+        if not self.modo_silencioso:
+            print(mensagem)
+
+    def _obter_tempo(self, inicio):
+        decorrido = (time.time() - inicio) / ESCALA_TEMPO
+        return f"{decorrido:05.2f}s"
